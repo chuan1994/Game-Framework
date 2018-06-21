bbdbd35a477acbb7425fee2076d74163581a712d
chuan wu <chuan1994@hotmail.com>
2018-06-21 23:05:10 +1200

    Added barebone BGM Controller

---
+++ Game Framework/Assets/Framework/Scripts/ControllerScripts/BGMController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class BGMController : MonoBehaviour {
+
+    [SerializeField]
+    AudioClip DefaultBackGroundMusic;
+
+    int currentLevel;
+
+    private static BGMController instance = null;
+
+    public static BGMController Instance {
+        get { return instance; }
+    }
+
+    // Use this for initialization
+    void Start() {
+        playMusic(DefaultBackGroundMusic);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        }
+        else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+        void playMusic(AudioClip clip) {
+        GetComponent<AudioSource>().clip = clip;
+        GetComponent<AudioSource>().Play();
+    }
+}
